a0c9323b8571976e25ad2a34ad61e57a9d235609
wankai313 <wankai0313@163.com>
2016-08-12 10:53:37 +0800

    Signed-off-by: wankai313 <wankai0313@163.com>

---
+++ Android_AutoLayout_Demo/src/com/wankai/android_autolayout_demo/activity/MainActivity.java
@@ -0,0 +1,8 @@
+package com.wankai.android_autolayout_demo.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+}
